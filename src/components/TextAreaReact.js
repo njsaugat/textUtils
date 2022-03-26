5fe77e99677b0c040236103e7575b6273a10fe16
njsaugat <njsaugat@gmail.com>
2022-03-26 18:13:03 +0545

    //components name has to start with capital letter always in react

--- src/components/TextAreaReact.js
+++ src/components/TextAreaReact.js
@@ -2,15 +2,16 @@
 import React from 'react';
 
 export default function TextAreaReact() {
+    //components name has to start with capital letter always in react
     return (
         <>
-        {/* <form>
-            <div class="form-group">
+        <form>
+            <div class="form-group m-2">
                 <label for="exampleFormControlTextarea1">Example textarea</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-        </form> */}
-        <h1>hello there</h1>
+        </form>
+        {/* <h1>hello there</h1> */}
         </>
     )
 }
